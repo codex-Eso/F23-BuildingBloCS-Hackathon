82d6d0432fab8731b9e387227a62210a804380a7
Jayden <2402279J@student.tp.edu.sg>
2025-12-12 16:56:16 +0800

    Add homepage code

--- hackathon/src/App.jsx
+++ hackathon/src/App.jsx
@@ -1,9 +1,15 @@
+import "../src/css/HomePage.css";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage.jsx"; 
+
 function App() {
   return (
     <>
-
+      <Routes>
+        <Route path="/Homepage" element={<HomePage />} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
